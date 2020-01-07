3b450dcd054feef0f7379327159403e6679bdd88
Joe Sudron <joesudron@outlook.com>
2020-01-07 16:52:20 +0000

    http.server

---
+++ js/my_script.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $("tr:odd").addClass("odd"); //changes colour of all odd <tr> class
+    $("tr:even").addClass("even"); //changes colour of all even <tr> class
+});
