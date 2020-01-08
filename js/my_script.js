e6ebb3fa7354d80a1077199292fb799dde190537
Joe Sudron <joesudron@outlook.com>
2020-01-08 14:03:55 +0000

    changed javascript fro selections

--- js/my_script.js
+++ js/my_script.js
@@ -1,4 +1,11 @@
-$(document).ready(function() {
-    $("tr:odd").addClass("odd"); //changes colour of all odd <tr> class
-    $("tr:even").addClass("even"); //changes colour of all even <tr> class
-});
+//will wait uuntil page ready
+ $(document).ready(function(){
+
+    /*when table header is clicked , removes selection class from all other table rows and
+      adds the selection class to this table headers row only*/
+     $("th").click(function(){
+     	$("tr").children().removeClass("selection")
+    	$(this).siblings().addClass("selection");
+
+  });
+ });
